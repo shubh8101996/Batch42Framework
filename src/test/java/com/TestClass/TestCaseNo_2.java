b7538a3ea87dc4808fcbc68a2f1c86998c0393ed
shubh8101996 <shubhamshedge8@gmail.com>
2022-09-19 21:36:38 +0530

    commited

---
+++ src/test/java/com/TestClass/TestCaseNo_2.java
@@ -0,0 +1,17 @@
+package com.TestClass;
+
+import org.testng.annotations.Test;
+
+import com.BaseClass.BaseClass;
+
+public class TestCaseNo_2 extends BaseClass {
+	
+	
+	@Test
+	public void testcase_2() {
+		
+		System.out.println("test case exce succesfully");
+		
+	}
+
+}
