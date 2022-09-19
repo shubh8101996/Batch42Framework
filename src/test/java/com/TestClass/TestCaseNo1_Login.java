9a6473a179b11c714c1f3e7f291c103c9f09e7f6
shubh8101996 <shubhamshedge8@gmail.com>
2022-09-19 22:16:47 +0530

    commited

--- src/test/java/com/TestClass/TestCaseNo1_Login.java
+++ src/test/java/com/TestClass/TestCaseNo1_Login.java
@@ -7,19 +7,30 @@ import org.testng.annotations.Test;
 import com.BaseClass.BaseClass;
 import com.PageClass.LoginPageClass;
 import com.Uitility.ExtendReportClass;
+import com.Uitility.Xls_Reader;
 
 @Listeners(ExtendReportClass.class)
 public class TestCaseNo1_Login extends BaseClass {
 
 	@Test
-	public void login() {
+	public void login() throws InterruptedException {
 
+//		Xls_Reader read=new Xls_Reader("C:\\Users\\shubh\\eclipse-workspace\\Batch42Framework\\src\\test\\resources\\testdata.xlsx");
+//		
+//		String username=read.getCellData("Sheet1", 0, 2);
+//		String password=read.getCellData("Sheet1", 1,2);
+//		
+//		System.out.println(username);
+//		System.out.println(password);
+		
+		
 		driver.get(mainUrl);
 		LoginPageClass lp = new LoginPageClass(driver);
 		lp.emailClear();
 		lp.passwordClear();
 //		Assert.assertTrue(false);
 		lp.enterCred(mainUsername, mainPass);
+		Thread.sleep(4000);
 		lp.clickLogbtn();
 
 	}
